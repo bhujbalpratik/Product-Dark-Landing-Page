1b1348a2ba7208094b4f9fc3193f1aa604513794
bhujbalpratik <pratikbhujbal743@gmail.com>
2024-10-03 18:42:42 +0530

    Logo Ticker section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <Banner />
       <Navbar />
       <Hero />
-      {/* 
       <LogoTicker />
+      {/* 
       <Features />
       <ProductShowcase />
       <FAQs />
--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,9 +1,10 @@
-import acmeLogo from "../assets/images/acme.png";
-import quantumLogo from "../assets/images/quantum.png";
-import echoLogo from "../assets/images/echo.png";
-import celestialLogo from "../assets/images/celestial.png";
-import pulseLogo from "../assets/images/pulse.png";
-import apexLogo from "../assets/images/apex.png";
+import acmeLogo from "../assets/images/acme.png"
+import quantumLogo from "../assets/images/quantum.png"
+import echoLogo from "../assets/images/echo.png"
+import celestialLogo from "../assets/images/celestial.png"
+import pulseLogo from "../assets/images/pulse.png"
+import apexLogo from "../assets/images/apex.png"
+import Image from "next/image"
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -12,8 +13,28 @@ const images = [
   { src: celestialLogo, alt: "Celestial Logo" },
   { src: pulseLogo, alt: "Pulse Logo" },
   { src: apexLogo, alt: "Apex Logo" },
-];
+]
 
 export const LogoTicker = () => {
-  return null;
-};
+  return (
+    <div className="bg-black text-white py-[4.5rem] sm:py-24">
+      <div className="container">
+        <h2 className="text-xl text-center text-white/70">
+          Preferred by the world&apos;s most cutting-edge teams
+        </h2>
+        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-20 after:w-20 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]  before:z-10 after:z-50 ">
+          <div className="flex gap-16">
+            {images.map(({ src, alt }, i) => (
+              <Image
+                src={src}
+                alt={alt}
+                key={i}
+                className="flex-none h-8 w-auto"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}

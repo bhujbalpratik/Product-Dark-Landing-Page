b905c2b921591d871e45f22d4a767b0d1bf23cc2
bhujbalpratik <pratikbhujbal743@gmail.com>
2024-10-03 21:32:36 +0530

    FAQ section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
       <LogoTicker />
       <Features />
       <ProductShowcase />
-      {/* 
       <FAQs />
+      {/* 
       <CallToAction />
       <Footer /> */}
     </>
--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,3 +1,10 @@
+"use client"
+
+import { useState } from "react"
+import PlusIcon from "../assets/icons/plus.svg"
+import MinusIcon from "../assets/icons/minus.svg"
+import clsx from "clsx"
+
 const items = [
   {
     question: "What payment methods do you accept?",
@@ -19,8 +26,47 @@ const items = [
     answer:
       "Security is our top priority. We use state-of-the-art encryption and comply with the best industry practices to ensure that your data is stored securely and accessed only by authorized users.",
   },
-];
+]
+
+const AccordionItem = ({
+  question,
+  answer,
+}: {
+  question: string
+  answer: string
+}) => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div
+      className="py-7 border-b border-white/30"
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <div className="flex items-center">
+        <span className="flex-1 text-lg font-bold cursor-pointer">
+          {question}
+        </span>
+        {isOpen ? <MinusIcon /> : <PlusIcon />}
+      </div>
+      <div className={clsx("mt-4", { hidden: !isOpen, "": isOpen === true })}>
+        {answer}
+      </div>
+    </div>
+  )
+}
 
 export const FAQs = () => {
-  return null;
-};
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24 bg-gradient-to-b from-[#5D2CA8] to-black">
+      <div className="container">
+        <h2 className="font-bold text-5xl sm:text-6xl sm:max-w-[648px] mx-auto text-center tracking-tighter">
+          Frequently asked questions
+        </h2>
+        <div className="mt-12 sm:max-w-[648px] mx-auto">
+          {items.map(({ question, answer }, i) => (
+            <AccordionItem question={question} answer={answer} key={i} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}

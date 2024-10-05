fe1349e0d70072730873402b1b1e453fed5f1a90
bhujbalpratik <pratikbhujbal743@gmail.com>
2024-10-05 18:17:45 +0530

    Features section animated

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,4 +1,7 @@
+"use client"
+import { useEffect, useRef } from "react"
 import EcoSystemIcon from "../assets/icons/ecosystem.svg"
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion"
 
 const features = [
   {
@@ -18,6 +21,48 @@ const features = [
   },
 ]
 
+const Feature = ({
+  title,
+  description,
+}: {
+  title: string
+  description: string
+}) => {
+  const border = useRef<HTMLDivElement>(null)
+  const offsetX = useMotionValue(-100)
+  const offsetY = useMotionValue(-100)
+  let maskImageB = useMotionTemplate`radial-gradient(100px 100px at ${offsetX}px ${offsetY}px, black, transparent)`
+  useEffect(() => {
+    const updateMousePosition = (e: MouseEvent) => {
+      if (!border.current) return
+      const borderRect = border.current.getBoundingClientRect()
+      offsetX.set(e.x - borderRect.x)
+      offsetY.set(e.y - borderRect.y)
+    }
+    window.addEventListener("mousemove", updateMousePosition)
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition)
+    }
+  }, [offsetX, offsetY])
+  return (
+    <div className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1 relative">
+      <motion.div
+        className="absolute border-2 border-purple-500 inset-0 rounded-xl"
+        ref={border}
+        style={{
+          WebkitMaskImage: maskImageB,
+          maskImage: maskImageB,
+        }}
+      ></motion.div>
+      <div className="inline-flex bg-white text-black h-14 w-14 justify-center items-center rounded-lg">
+        <EcoSystemIcon />
+      </div>
+      <h3 className="mt-6 font-bold">{title}</h3>
+      <p className="mt-2 text-white/70">{description}</p>
+    </div>
+  )
+}
+
 export const Features = () => {
   return (
     <div className="bg-black text-white py-[4.5rem] sm:py-24">
@@ -34,16 +79,7 @@ export const Features = () => {
         </div>
         <div className="mt-16 flex flex-col sm:flex-row gap-4">
           {features.map(({ title, description }, i) => (
-            <div
-              key={i}
-              className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1"
-            >
-              <div className="inline-flex bg-white text-black h-14 w-14 justify-center items-center rounded-lg">
-                <EcoSystemIcon />
-              </div>
-              <h3 className="mt-6 font-bold">{title}</h3>
-              <p className="mt-2 text-white/70">{description}</p>
-            </div>
+            <Feature title={title} description={description} key={i} />
           ))}
         </div>
       </div>
